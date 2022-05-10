2a1eeb209be4fe2ddacc4b41c92caa5cd468ee3b
Michael Ammann <mazgch@users.noreply.github.com>
2022-05-10 20:35:19 +0200

    Update SparkFun_u-blox_SARA-R5_Arduino_Library.cpp

--- src/SparkFun_u-blox_SARA-R5_Arduino_Library.cpp
+++ src/SparkFun_u-blox_SARA-R5_Arduino_Library.cpp
@@ -122,7 +122,7 @@ bool SARA_R5::begin(SoftwareSerial &softSerial, unsigned long baud)
 }
 #endif
 
-bool SARA_R5::begin(HardwareSerial &hardSerial, unsigned long baud, bool doBegin)
+bool SARA_R5::begin(HardwareSerial &hardSerial, unsigned long baud)
 {
   if (NULL == _saraRXBuffer)
   {
@@ -163,9 +163,9 @@ bool SARA_R5::begin(HardwareSerial &hardSerial, unsigned long baud, bool doBegin
   SARA_R5_error_t err;
 
   _hardSerial = &hardSerial;
-  if (doBegin) {
-    _hardSerial->begin(baud);
-  }
+
+  _hardSerial->begin(baud)
+
   err = init(baud);
   if (err == SARA_R5_ERROR_SUCCESS)
   {
@@ -5976,7 +5976,7 @@ void SARA_R5::beginSerial(unsigned long baud)
   delay(100);
   if (_hardSerial != NULL)
   {
-    _hardSerial->updateBaudRate(baud);
+    _hardSerial->begin(baud);
   }
 #ifdef SARA_R5_SOFTWARE_SERIAL_ENABLED
   else if (_softSerial != NULL)
